7119b3e9b458891cd5bafe80a3cbbf45fdc52321
Rony <50582705+rony9707@users.noreply.github.com>
2025-03-01 15:56:55 +0530

    email fix

--- common/emailConfig.js
+++ common/emailConfig.js
@@ -1,21 +1,19 @@
-require('dotenv').config()
-const nodemailer = require('nodemailer')
+require('dotenv').config();
+const nodemailer = require('nodemailer');
 
-//Declare Env Variables here
-myEmail = process.env.email
-myPassword = process.env.password
+// Declare Env Variables here
+const myEmail = process.env.email;
+const myPassword = process.env.password;
 
-
-//mail config
-let config = {
-  service: 'gmail',
-  auth: {
-    user: myEmail,
-    pass: myPassword
-  }
-}
-
-let transporter = nodemailer.createTransport(config)
-
+// Create transporter with explicit host/port (recommended)
+let transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,                // Use 465 for SSL
+    secure: true,              // true for 465, false for 587
+    auth: {
+        user: myEmail,
+        pass: myPassword       // This must be an App Password if using 2FA
+    }
+});
 
 module.exports = transporter;
